65f5a877b34d681ad15f887b7ffcb5cf6804e587
Ibar <ibarvivas@gmail.com>
2025-10-14 10:13:52 +0200

    Terminando de unir rama_JMB

--- .web/vite.config.js
+++ .web/vite.config.js
@@ -46,6 +46,10 @@ export default defineConfig((config) => ({
   build: {
     assetsDir: "/assets".slice(1),
     rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === "EVAL" && warning.id && warning.id.endsWith("state.js")) return;
+        warn(warning);
+      },
       jsx: {},
       output: {
         advancedChunks: {
